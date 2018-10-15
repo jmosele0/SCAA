2e5117fef0890803a7394e7ce4efd4e156daa610
jmosele0 <31440430+jmosele0@users.noreply.github.com>
2018-10-15 23:15:24 +0800

    Loading from JSON file

--- main.lua
+++ main.lua
@@ -20,7 +20,47 @@ local composer = require "composer"
     -- }
 -- }
 
+local json = require( "json" )
+
+function loadFromJson( filename, base )
+	
+	-- set default base dir if none specified
+	if not base then base = system.DocumentsDirectory; end
+
+	-- create a file path for corona i/o
+	local path = system.pathForFile( filename, base )
+
+	-- will hold contents of file
+	local file, contents
+
+	-- io.open opens a file at path. returns nil if no file found
+	if path then
+		file = io.open( path, "r" )
+	end
+
+	if file then
+	    -- read all contents of file into a string
+	    contents = file:read( "*a" )
+	    io.close( file )-- close the file after using it
+		print("x")
+		
+	else
+		--if not found then return a string representing an empty table
+		contents = "[]"
+		print("x")
+	end
+
+	--convert the json string to a table
+	local returnTable = json.decode(contents)
+
+	--return the table
+	return returnTable
+end
+
 -- Add any system wide event handlers, location, key events, system resume/suspend, memory, etc.
 
 -- load scene1
+-- Add any system wide event handlers, location, key events, system resume/suspend, memory, etc.
+
+_G.countryData = loadFromJson("Country info.json", system.DocumentsDirectory)
 composer.gotoScene( "searchScene" )
